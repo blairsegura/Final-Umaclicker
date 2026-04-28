23d760ffefff12e7a794f3fc6e984dfd7def7bb1
Blair Segura <bts220001@utdallas.edu>
2026-04-28 14:46:00 -0500

    Implement miles tracking and display feature
    Add functionality to track and display miles ran.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
 // main.js
 
 console.log("Hello, World!");
+
+
+let umaButton = document.getElementById("umaButton")
+let umaDisplay = document.getElementById("umaDisplay")
+
+let miles=0
+let multiplier=1
+let multiplerCost=25
+
+function displayMilesAmt(){
+    umaDisplay.innerText = "You have ran " +miles+" miles."
+}
+
+umaButton.addEventListener('click',function(){
+   miles+=multiplier
+   displayMilesAmt()
+})
